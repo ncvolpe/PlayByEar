75441deb73ac95e159fd1f3058fb747402c82d10
jasmrutt <jnr7@njit.edu>
2024-07-15 14:40:11 -0400

    code for interface and back end

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import {Piano} from './components/Piano';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Piano />
+      </header>
+    </div>
+  );
+}
+
+export default App;
